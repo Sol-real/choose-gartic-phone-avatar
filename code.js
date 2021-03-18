3443cc9454b8c878d13bd2d3082da3a63012e07c
Sol_ <37932436+Sol-real@users.noreply.github.com>
2021-03-19 00:48:36 +0100

    Quality of Life
    Added that if non sense is added the avatar does not change

--- code.js
+++ code.js
@@ -1,14 +1,15 @@
 function changeAvatar() {
     let ava_num = prompt("Please choose the number you want to change your Avatar to!", "");
     if (isNaN(parseInt(ava_num))) {
-        
           ava_num = localStorage.getItem("avatar");
     } else {
         num = parseInt(ava_num);
         if (num > 34) {
-            num = num - 35;
+            ava_num = localStorage.getItem("avatar");
         }
+        else {
         ava_num = String(num);
+        }
     }
     localStorage.setItem("avatar", ava_num);
 };
