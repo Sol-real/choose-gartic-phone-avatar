3f4091e7f9ae9dfd61c0ecfd139a719b887aff65
Sol_ <37932436+Sol-real@users.noreply.github.com>
2021-03-19 00:38:10 +0100

    Added Avatar Change Code

---
+++ code.js
@@ -0,0 +1,16 @@
+function changeAvatar() {
+    let ava_num = prompt("Please choose the number you want to change your Avatar to!", "");
+    if (isNaN(parseInt(ava_num))) {
+        
+          ava_num = "1";
+    } else {
+        num = parseInt(ava_num);
+        if (num > 34) {
+            num = num - 35;
+        }
+        ava_num = String(num);
+    }
+    localStorage.setItem("avatar", ava_num);
+};
+changeAvatar();
+location.reload(false);
