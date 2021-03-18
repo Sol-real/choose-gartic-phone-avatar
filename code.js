bd28b42bd8bd28aa177ad39b90b67389b92d8087
Sol_ <37932436+Sol-real@users.noreply.github.com>
2021-03-19 00:44:58 +0100

    Update code.js

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ function changeAvatar() {
     let ava_num = prompt("Please choose the number you want to change your Avatar to!", "");
     if (isNaN(parseInt(ava_num))) {
         
-          ava_num = "1";
+          ava_num = localStorage.getItem("avatar");
     } else {
         num = parseInt(ava_num);
         if (num > 34) {
